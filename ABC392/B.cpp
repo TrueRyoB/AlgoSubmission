c5b1707c50cf69869e5927bde2fae646df96536f
ryojiaraki <ryorar.work@gmail.com>
2025-07-10 21:55:28 +0900

    upsolve

--- ABC392/B.cpp
+++ ABC392/B.cpp
@@ -6,16 +6,14 @@ using namespace atcoder;
 
 int32_t main() {
     int N, M; cin>>N>>M;
-    vector<bool> v(N+1, false); v[0]=true;
+    vector<bool> v(N+1, false);
     for(int i=0; i<M; ++i) {
         int x; cin>>x;
         v[x]=true;
     }
     cout << (N-M) << endl;
-    for(int i=0; i<N; ++i) {
-        if(!v[i]) cout << i << " ";
+    for(int i=1; i<=N; ++i) {
+        if(!v[i]) cout << (i) << " ";
     }
-    //C以降もupsolveする
-
     return 0;
 }
--- ABC392/C.cpp
+++ ABC392/C.cpp
@@ -1,13 +1,22 @@
 #include <bits/stdc++.h>
-#include <atcoder/all>
+// #include <atcoder/all>
 using namespace std;
-using namespace atcoder;
+// using namespace atcoder;
 #define int long long
 
 int32_t main() {
     int N; cin>>N;
-    
+
+    vector<int> p(N+1);
+    for(int i=1; i<=N; ++i) cin>>p[i];
+    vector<int> q(N+1); 
+    for(int i=1; i<=N; ++i) cin>>q[i];
+    vector<int> ztop(N+1);
+    for(int i=1; i<=N; ++i) ztop[q[i]] = i;
+
+    for(int i=1; i<=N; ++i) {
+        cout << q[p[ztop[i]]] << " ";
+    }
 
     return 0;
-}
-//Q -> P
+}//g++ -Wall -Wextra -g -std=c++20 ABC392/C.cpp -o C
