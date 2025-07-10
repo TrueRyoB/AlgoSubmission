b4bb42306d4a62e64aafa192bd5772d7595d43b3
ryojiaraki <ryorar.work@gmail.com>
2025-07-10 22:20:46 +0900

    解説AC

--- ABC392/D.cpp
+++ ABC392/D.cpp
@@ -1,10 +1,49 @@
 #include <bits/stdc++.h>
-#include <atcoder/all>
 using namespace std;
-using namespace atcoder;
+ 
 #define int long long
+ 
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+ 
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define BUFF ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+ 
+const int INF = 1e18+5;
 
-int32_t main() {
+signed main()
+{
+    BUFF;
+    int n,num; cin >> n;
 
-    return 0;
+    vi k(n);
+    vector<map<int,int>> used(n);
+    for(int i = 0; i < n; i++) {
+        cin >> k[i];
+        for(int j = 0; j < k[i]; j++) {
+            cin >> num;
+            used[i][num]++;
+        }
+    }
+
+    double res = 0.0;
+    for(int i = 0; i < n; i++) {
+        for(int j = i+1; j < n; j++) {
+            double aux = 0;
+            for(auto [u, times] : used[i]) {
+                if(used[j].find(u) != used[j].end()) {
+                    aux += ((double)1.0*used[j][u]/k[j])*((double)1.0*times/k[i]);
+                }
+            }
+
+            res= max(res, aux);
+        }
+    }
+
+    cout << fixed << setprecision(15) << res << endl;
+
+    return 0; 
 }
+//https://atcoder.jp/contests/abc392/submissions/67473440
