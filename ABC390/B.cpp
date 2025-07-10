81c64be7840661592e4d1bff6fb9781a7e161e9c
ryojiaraki <ryorar.work@gmail.com>
2025-07-11 00:02:42 +0900

    2025/07/11 精進

--- ABC390/B.cpp
+++ ABC390/B.cpp
@@ -5,6 +5,17 @@ using namespace atcoder;
 #define int long long
 
 int32_t main() {
+    int N;cin>>N;
+    vector<int> v(N); for(auto& e:v) cin>>e;
+
+    bool safe = true;
+    for(int i=1; i+1<N; ++i) {
+        if(v[i]*v[i] != v[i-1] * v[i+1]) {
+            safe=false; break;
+        }
+    }
+    if(safe) cout << "Yes";
+    else cout << "No";
 
     return 0;
-}
+}//まさかのtypoで(泣く)
