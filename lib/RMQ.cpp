bb9eb8ed7fb2559eb5f3424362deb54d1a8db763
trueryob <trueryob@proton.me>
2025-11-30 06:43:24 -0600

    fix: 実装をさぼったライブラリのファイルをopt out

--- lib/RMQ.cpp
+++ lib/RMQ.cpp
@@ -1,1 +1,27 @@
-//動かないので封印
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T, typename F>
+struct LazySegTree {
+  int n;
+  vector<T> dat, lazy;
+  T id, lazy_id;
+  F op;
+
+  LazySegTree(int n_, T id_, F op_, T lazy_id_) : id(id_), op(op_), lazy_id(lazy_id_) {
+    int x=1;
+    while(x<n_) x<<=1;
+    n=x;
+    dat.assign(2*n-1, id);
+    lazy.assign(2*n-1, lazy_id);
+  }
+  void set(int p, T x) {
+
+  }
+  T get(int p) {
+
+  }
+  T prod(int l, int r) {
+
+  }
+};
