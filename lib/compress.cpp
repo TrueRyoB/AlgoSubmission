dd59308f3adaf5e78853cee584bc12c703240643
trueryob <trueryob@proton.me>
2025-12-06 12:34:18 -0600

    fix:参照先変数間違いのバグを修正

--- lib/compress.cpp
+++ lib/compress.cpp
@@ -3,8 +3,7 @@ using namespace std;
 using ll = long long;
 
 vector<ll> compress(vector<ll> v) {
-  vector<ll> res = v;
   sort(v.begin(), v.end());
-  res.erase(unique(res.begin(), res.end()), res.end());
-  return res;
+  v.erase(unique(v.begin(), v.end()), v.end());
+  return v;
 }
