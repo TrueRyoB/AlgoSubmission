eb1e81abd05a1fde9233eac118580a06c72fd5fe
trueryob <trueryob@proton.me>
2025-12-24 16:51:12 -0600

    feat: implemented more of compression for an easy reference

--- lib/compress.cpp
+++ lib/compress.cpp
@@ -7,3 +7,15 @@ vector<ll> compress(vector<ll> v) {
   v.erase(unique(v.begin(), v.end()), v.end());
   return v;
 }
+
+int main() {
+
+  int N; cin>>N;
+  vector<ll> v(N); for(auto& e:v) cin>>e;
+  vector<int> p;
+  {
+    vector<ll> c=compress(v);
+    for(int i=0; i<N; ++i) p.push_back(lower_bound(c.begin(), c.end(), v[i])-c.begin());
+  }
+  return 0;
+}
