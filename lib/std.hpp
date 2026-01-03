67ce8afcebd33a3ab3a48e064ffd372bb1679db2
trueryob <trueryob@proton.me>
2026-01-03 11:59:18 -0600

    feat: moved chunks of headers from main.cpp for clarity

---
+++ lib/std.hpp
@@ -0,0 +1,20 @@
+#ifndef LIB_STD
+#define LIB_STD
+
+#include <vector>
+#include <iostream>
+#include <numeric>
+#include <cmath>
+#include <functional>
+#include <cstdint>
+#include <string>
+#include <cassert>
+
+using namespace std;
+using ll = long long;
+
+constexpr ll LINF = 1001001001001001001ll;
+constexpr int d[] = {0, -1, 0, 1, 0};
+template <typename T> inline void chmin(T& a, const T& b) {if (a > b) a = b; }
+template <typename T> inline void chmax(T& a, const T& b) {if (a < b) a = b; }
+#endif
