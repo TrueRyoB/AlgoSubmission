ef5a0e6c1d65f912c4315b848a5efa9720db14cb
trueryob <trueryob@proton.me>
2026-01-03 12:52:11 -0600

    fix: minor issue

--- lib/cout.hpp
+++ lib/cout.hpp
@@ -3,8 +3,8 @@
 
 #include "std.hpp"
 
-template <typename T> ostream& operator<<(ostream& os, const vector<T>& v) { for(const auto& e:v)os<<e<<'\t'; return os; }
-template <typename T, size_t N> ostream& operator<<(ostream& os, const array<T, N>& v) { os << '['; for(size_t i=0; i<N; ++i) { if(i) os<<", "; os<<v[i]; } cos << "]"; return os; }
+namespace std {
+template <typename T, size_t N> ostream& operator<<(ostream& os, const array<T, N>& v) { os << '['; for(size_t i=0; i<N; ++i) { if(i) os<<", "; os<<v[i]; } os << "]"; return os; }
 template <typename T> ostream& operator<<(ostream& os, const pair<T, T>& p) { os << '{' << p.first << ", " << p.second <<  "}"; return os; }
-
+}
 #endif
