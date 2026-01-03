18903b9c83d0c1e7f117ef22056d433de7bda91a
trueryob <trueryob@proton.me>
2026-01-03 12:00:44 -0600

    fix: modified the template code based on the current bundling system

--- lib/template.cpp
+++ lib/template.cpp
@@ -1,23 +1,11 @@
 #pragma GCC optimize("O3")
-#include <bits/stdc++.h>
-using namespace std;
-using ll = long long;
-// #include <atcoder/all>
-// using namespace atcoder;
-// using mint = modint1000000007;
+#ifdef LOCAL
+#include "lib/std.hpp"
+#endif
 
-constexpr int INF = 1001001001;
-constexpr ll LINF = 1001001001001001001ll;
-constexpr int di[] = {0, -1, 0, 1};
-constexpr int dj[] = {1, 0, -1, 0};
-
-template <typename T> inline bool chmin(T& a, const T& b) {bool compare = a > b; if (a > b) a = b; return compare;}
-template <typename T> inline bool chmax(T& a, const T& b) {bool compare = a < b; if (a < b) a = b; return compare;}
-template <typename T> ostream& operator<<(ostream& os, const vector<T>& v) { for (const auto& e:v) os << e << " "; return os; }
-
-int32_t main() {
+int main() {
   ios::sync_with_stdio(false);
-	cin.tie(nullptr);
+  cin.tie(nullptr);
   
 
   return 0;
