12a8fecf4da36ad4bd904afa1a5d5c9dc12b03f8
trueryob <trueryob@proton.me>
2026-01-03 12:08:12 -0600

    feat: methods and overrides for a convenient iostream

--- lib/std.hpp
+++ lib/std.hpp
@@ -17,4 +17,8 @@ constexpr ll LINF = 1001001001001001001ll;
 constexpr int d[] = {0, -1, 0, 1, 0};
 template <typename T> inline void chmin(T& a, const T& b) {if (a > b) a = b; }
 template <typename T> inline void chmax(T& a, const T& b) {if (a < b) a = b; }
+
+#define endl '\n'
+template <typename T> ostream& operator<<(ostream& os, const vector<T>& v) { for(const auto& e:v)os<<e<<'\t'; return os; }
+
 #endif
