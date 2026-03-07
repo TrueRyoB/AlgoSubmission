d3bea8bf1519f726497d5063cef442a4bfa48f6f
trueryob <trueryob@proton.me>
2026-03-07 15:33:59 -0600

    fix: some issues

--- lib/std.hpp
+++ lib/std.hpp
@@ -26,16 +26,4 @@ template <typename T> inline void chmax(T& a, const T& b) {if (a < b) a = b; }
 #define endl '\n'
 template <typename T> ostream& operator<<(ostream& os, const vector<T>& v) { for(const auto& e:v)os<<e<<'\t'; return os; }
 
-template<typename T> inline int sz(const vector<T>&v) { return (int)v.size(); }
-inline int sz(const string&v) { return (int)v.size(); }
-template<typename T> inline int sz(const set<T>&v) { return (int)v.size(); }
-
-constexpr ll pow10(int n) {
-  ll x=1;
-  while(n--) x*=10;
-  return x;
-}
-
-#define mint Modint<MOD>
-
 #endif
--- rem.cpp
+++
@@ -1,9 +0,0 @@
-/*
-
-UnionFind
-集合
-包除原理: DPで求める https://atcoder.jp/contests/dp/tasks/dp_y
-LIS
-幅優先探索
-
-*/
